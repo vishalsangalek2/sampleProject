a237fb35ff8f1467bd805fab2dd2254403eb6593
vishalsangalek2 <vishalsangalek2@gmail.com>
2022-10-26 14:32:56 +0900

    first commit

---
+++ eclipse-workspace/Practice/src/Trial/Data.java
@@ -0,0 +1,12 @@
+package Trial;
+
+public class Data {
+	public static void data() {
+		Encapsulation en = new Encapsulation();
+		en.setAccount_Holder("Abhijit Sangale");
+		en.setAccount_Number(123456789);
+		en.setBalance(852489632.00);
+		en.setBank_Name("State bank of india");
+		}
+
+}
